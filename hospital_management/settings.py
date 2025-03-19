de692d8c9139e797411ebe8028e6fd78c172221d
eria-developer <eriddeveloper@gmail.com>
2025-03-19 22:38:13 +0300

    changed db to postgresql

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -89,10 +89,26 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medic_plus',
+        'USER': 'cyber',
+        'PASSWORD': 'Cyber@2001',
+        # 'HOST': '127.0.0.1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=public,shared'
+        }
+       
     }
 }
 
--- suppliers/models.py
+++ suppliers/models.py
@@ -5,8 +5,12 @@ class Supplier(models.Model):
     Model representing a supplier in the hospital management system.
     """
     name = models.CharField(max_length=100)
-    contact_info = models.TextField(blank=True)
+    registration_number = models.CharField(max_length=30, unique=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    alternative_phone = models.CharField(max_length=15, null=True)
     address = models.TextField(blank=True)
+    description = models.TextField(null=True)
 
     def __str__(self):
         """

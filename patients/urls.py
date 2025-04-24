7aa14e659fef07a4e8bb9a661d09e4b318cbd645
eria-developer <eriddeveloper@gmail.com>
2025-04-24 17:46:32 +0300

    updated patients urls

--- patients/urls.py
+++ patients/urls.py
@@ -3,5 +3,5 @@ from patients.views import PatientListCreateView, PatientDetailView
 
 urlpatterns = [
     path('', PatientListCreateView.as_view(), name='patient-list-create'),
-    path('patients/<int:pk>/', PatientDetailView.as_view(), name='patient-detail'),
+    path('<int:pk>/', PatientDetailView.as_view(), name='patient-detail'),
 ]
